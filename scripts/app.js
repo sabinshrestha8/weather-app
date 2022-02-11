0a70aff732d727967eb48a626c7e07ea142b0943
sabin <sabinshrestha8888@gmail.com>
2022-02-11 13:37:29 +0545

    updating images & icons in dynamic way

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,11 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector(".time");
+const icon = document.querySelector(".icon img");
 
 const updateUI = (data) => {
-    // console.log(data);
+    console.log(data);
     // const cityDetails = data.cityDetails;
     // const weather = data.weather;
 
@@ -21,6 +23,22 @@ const updateUI = (data) => {
             <span>&deg;C</span>
         </div>
     `;
+
+    // update the night/day & icon images
+
+    // update weather icons dynamically using template string
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute("src", iconSrc);
+
+    // overwrite the img a/c to location time(Day || Night)
+    let timeSrc = null;
+    if (weather.IsDayTime) {
+        timeSrc = "img/day.svg";
+    } else {
+        timeSrc = "img/night.svg";
+    }
+
+    time.setAttribute("src", timeSrc);
 
     // remove the d-none class if present
     if (card.classList.contains("d-none")) {
